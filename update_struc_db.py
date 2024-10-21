f3f944756fc873bb247caa123ab51ce3c9765e7b
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-10-21 12:53:45 +0200

    added unsafe mode to fix broken row

--- update_struc_db.py
+++ update_struc_db.py
@@ -22,7 +22,7 @@ def main(db_index: int, txt_dir: str, db_dir: str, relaxed: bool = False, new_co
     if os.path.basename(txt_dir) == 'OUTCAR': updated_atoms: Atoms = read(txt_dir, index=-1, format='vasp-out')
     elif os.path.basename(txt_dir) == 'CONTCAR': updated_atoms: Atoms = read(txt_dir, format='vasp')
     else: updated_atoms: Atoms = read(txt_dir, index=-1)
-    if not new_constraint: updated_atoms.set_constraint(constraints)
+    if not new_constraint and not unsafe: updated_atoms.set_constraint(constraints)
     if not unsafe: assert old_atoms.get_chemical_formula() == updated_atoms.get_chemical_formula()
     update_db(db_dir,
               dict(
@@ -44,4 +44,4 @@ if __name__ == '__main__':
     parser.add_argument('-unsafe', action='store_true', default=False)
     args = parser.parse_args()
 
-    main(args.db_index, args.txt_directory, args.db_directory, args.relaxed, args.new_constraint)
+    main(args.db_index, args.txt_directory, args.db_directory, args.relaxed, args.new_constraint, unsafe=args.unsafe)

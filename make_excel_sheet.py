8682f0b03550ee73dd50db35e15be96c4beb7679
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-11-04 12:40:04 +0100

    try conditonal formmating

--- make_excel_sheet.py
+++ make_excel_sheet.py
@@ -93,7 +93,7 @@ def main(molecule_database_dir: str, solid_database_dir: str, verbose: bool = Fa
         work_sheet.cell(i+2, len(functional_list) + 2, reac.experimental_ref)
     deviation_sheet.conditional_formatting.add(f'B2:{xl.utils.cell.get_column_letter(j)}{i}',
                                                ColorScaleRule(start_type='formula',
-                                                              start_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i}),MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
+                                                              start_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i});MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
                                                               start_color='AA0000',
 
                                                               mid_type='num',
@@ -101,7 +101,7 @@ def main(molecule_database_dir: str, solid_database_dir: str, verbose: bool = Fa
                                                               mid_color='FFFFFF',
 
                                                               end_type='formula',
-                                                              end_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i}),MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
+                                                              end_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i});MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
                                                               end_color='AA0000'
                                                               ))
 
@@ -116,7 +116,7 @@ def main(molecule_database_dir: str, solid_database_dir: str, verbose: bool = Fa
         formation_sheet.cell(i+2, len(functional_list) + 2, reac.experimental_ref)
     formation_sheet_deviation.conditional_formatting.add(f'B2:{xl.utils.cell.get_column_letter(j)}{i}',
                                                ColorScaleRule(start_type='formula',
-                                                              start_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i}),MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
+                                                              start_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i});MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
                                                               start_color='AA0000',
 
                                                               mid_type='num',
@@ -124,7 +124,7 @@ def main(molecule_database_dir: str, solid_database_dir: str, verbose: bool = Fa
                                                               mid_color='FFFFFF',
 
                                                               end_type='formula',
-                                                              end_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i}),MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
+                                                              end_value=f'=-MAX(-MIN(B2:{xl.utils.cell.get_column_letter(j)}{i});MAX(B2:{xl.utils.cell.get_column_letter(j)}{i}))',
                                                               end_color='AA0000'
                                                               ))
 
